d6e2a2a441ad76984364efa986f60dc0a0d50baf
bharathreddybiradar-code <bharathreddybiradar@gmail.com>
2026-09-25 18:21:24 +0530

    Time: 3 ms (29.83%) | Memory: 8.6 MB (20.31%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0;
+        while(x>0 || x<0){
+         int lastdigit=x%10;
+         rev=rev * 10+lastdigit;
+         x=x/10;
+        }
+        if(rev>INT_MAX || rev<INT_MIN){
+        return 0;
+    }
+    return rev;
+    }
+};
