59a86a62dc554aaaf51a1a0d66490645cf37ac6c
bharathreddybiradar-code <bharathreddybiradar@gmail.com>
2026-09-25 18:02:53 +0530

    Time: 19 ms (9.08%), Space: 8.7 MB (13.77%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<0){
+            return 0;
+        }
+        int ans=0;
+    for(long i=0;i*i<=x;i++){
+        ans=i;
+        }
+    return ans;
+    }
+};
